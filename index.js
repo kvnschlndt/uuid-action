2e4bc239cb8825df13b1001427c96e7492beadda
kvnschlndt <k.schlundt@1inch.io>
2024-06-28 11:13:23 +0200

    check for GITHUB_OUTPUT variable

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const uuidv1 = require('uuid/v1');
 const uuidv5 = require('uuid/v5');
 const core = require('@actions/core');
+const process = require('process')
 
 async function run() {
   try {
+    /* https://github.com/actions/toolkit/issues/1336 use new way of OUTPUT */
+    if (!process.env.GITHUB_OUTPUT) {
+      process.env.GITHUB_OUTPUT = true
+      core.info(`GITHUB_OUTPUT env was not set, it was set to ${process.env.GITHUB_OUTPUT}`)
+    }
+
     const name = core.getInput('name');
     const namespace =
       core.getInput('namespace') || 'f9962f80-1514-11ea-bfe7-1f9e6cf0a044';
